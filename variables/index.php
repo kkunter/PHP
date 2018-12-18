97265af586614f43c84f68dee70cd555eca6bd69
Künter <kunter.partel@khk.ee>
2018-12-18 12:57:01 +0200

    PHP Variables

---
+++ variables/index.php
@@ -0,0 +1,29 @@
+<?php
+
+
+// Operaatorid
+
+
+$arv1 = 7;
+$arv2 = 24;
+
+
+echo '<h4>Matemaatilised operaatorid</h4>'.'<br>';
+
+echo $arv1. ' + '.' = '.($arv1 + $arv2).'<br>';
+echo $arv1. ' - '.' = '.($arv1 - $arv2).'<br>';
+echo $arv1. ' * '.' = '.($arv1 * $arv2).'<br>';
+echo $arv1. ' / '.' = '.($arv1 / $arv2).'<br>';
+echo $arv1. ' % '.' = '.($arv1 % $arv2).'<br>';
+
+
+echo '<hr>';
+
+echo '<h4>Võrdlusoperaatorid</h4>'.'<br>';
+
+echo $arv1.' < '.arv2.' See on '.($arv1 < $arv2).'<br>';
+echo $arv1.' > '.arv2.' See on '.($arv1 > $arv2).'<br>';
+echo $arv1.' <= '.arv2.' See on '.($arv1 <= $arv2).'<br>';
+echo $arv1.' >= '.arv2.' See on '.($arv1 >= $arv2).'<br>';
+echo $arv1.' == '.arv2.' See on '.($arv1 == $arv2).'<br>';
+echo $arv1.' != '.arv2.' See on '.($arv1 != $arv2).'<br>';
